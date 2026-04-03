a81fb49fc4696cda7f71b3d54f89287b793f577d
AsmiKesharkar16 <asmikesharkar@gmail.com>
2026-04-03 11:57:28 +0530

    Add files via upload

---
+++ JSON file handling/json_to_csv.py
@@ -0,0 +1,24 @@
+import json
+import csv
+
+# Step 1: Open and read JSON file
+json_file = open("JSON file handling/students.json", "r")
+data = json.load(json_file)
+json_file.close()
+
+# Step 2: Open CSV file for writing
+csv_file = open("JSON file handling/students.json", "w", newline='')
+
+# Step 3: Get headers from JSON keys
+headers = data[0].keys()
+
+writer = csv.DictWriter(csv_file, fieldnames=headers)
+
+# Step 4: Write header and rows
+writer.writeheader()
+writer.writerows(data)
+
+# Step 5: Close CSV file
+csv_file.close()
+
+print("Conversion completed! Check students.csv file.")
